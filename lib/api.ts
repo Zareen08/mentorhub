a53923837399400a30a2e63626ff2a5cac10c5f9
Farhat Zareen <farhat.songi@gmail.com>
2026-05-23 19:53:33 +0600

    api

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api/v1';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const api = axios.create({
   baseURL: API_URL,
